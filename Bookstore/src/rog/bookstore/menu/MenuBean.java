3c5ace12858adc8d6c96f8ba76d870cce8e73698
RogImeri <artur.rog@imeri.pl>
2017-08-24 17:01:09 +0200

    Bootsface

---
+++ Bookstore/src/rog/bookstore/menu/MenuBean.java
@@ -0,0 +1,12 @@
+package rog.bookstore.menu;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name="menuBean")
+@SessionScoped
+public class MenuBean implements Serializable{
+
+}
---
+++ Bookstore/src/rog/bookstore/servlets/MainServlet.java
@@ -0,0 +1,43 @@
+package rog.bookstore.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainServlet
+ */
+@WebServlet({ "/MainServlet", "/start" })
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MainServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("jestem w servlecie");
+		request.setAttribute("test", "Moja ukryta wiadomosc");
+		request.getRequestDispatcher("/pages/index.xhtml").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
